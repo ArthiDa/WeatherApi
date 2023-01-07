8b56f28b448431009b6dcc56e70491bd9ac652ff
ArthiDa <arthida150@gmail.com>
2023-01-08 02:15:17 +0600

    Enter Press Fix

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const token = `3001eea91d1e4d7e30947e2f5c99de55`
 const searchClick = ()=>{
     const value = document.getElementById("inputText").value;
-
     const link = `https://api.openweathermap.org/data/2.5/weather?q=${value}&appid=${token}`;
     fetch(link)
     .then(res=>res.json())
     .then(data => displayDetails(data))
 }
+
+document.getElementById("inputText").addEventListener("keypress",function(event){
+    // console.log(event);
+    if(event.key=='Enter'){
+        event.preventDefault();
+        searchClick();
+    }
+})
 
 const displayDetails = (data)=>{
     document.getElementById('cityName').innerText = data.name;
